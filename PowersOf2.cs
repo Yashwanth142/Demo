b9e44b46c82cc08bfe81552feaf1b76d37b08696
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-24 11:28:21 +0530

    added power of 2's

---
+++ PowersOf2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class PowersOf2
+    {
+        public static void Tableof2powers()
+        {
+            Console.WriteLine("Enter a number in range 0 to 31 :");
+            int n= Convert.ToInt32(Console.ReadLine());
+
+            if (n < 32)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("2 power of " + i + " is " + Math.Pow(2, i));
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number is above the range");
+            }
+        }
+    }
+}
