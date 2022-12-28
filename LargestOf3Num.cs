7690a65491cf180b8400295d9d14c78e6ad2c503
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-28 12:12:19 +0530

    Largest among 3 num added

---
+++ LargestOf3Num.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class LargestOf3Num
+    {
+        public static void LargestNum() 
+        {
+            Console.WriteLine("Enter the first number :");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number :");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third number");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if(a>b & a>c)
+            {
+                Console.WriteLine("The Largest number is " + a);
+            }
+            else if(b>c & b>a)
+            {
+                Console.WriteLine("The Largest number is " + b);
+            }
+            else
+            {
+                Console.WriteLine("The Largest number is " + c);
+            }
+        }
+    }
+}
