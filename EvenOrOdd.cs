edc5a3f4980b1ecf62c5f63432c9e82fb2ddcb0d
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-28 12:08:52 +0530

    Check Even or Odd added

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class EvenOrOdd
+    {
+        public static void CheckEvenorOdd()
+        {
+            Console.WriteLine("Enter a number :");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            if(num %2 == 0) 
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
